8060fb5da5eb13246ab73802c542191c90380e4b
Craig <indigomx9@gmail.com>
2022-06-28 15:34:46 -0400

    Created the CRUD methods.

--- src/controllers/PlayerCon.js
+++ src/controllers/PlayerCon.js
@@ -26,6 +26,54 @@ class PlayerClass {
             next(error);
         }
     };
+
+    GetOne = async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const getOneQry = `
+                SELECT * FROM players
+                WHERE id = $1
+            `;
+            const values = [id];
+            const player = await pool.query(getOneQry, values);
+            res.json(player.rows[0]);
+        } catch (error) {
+            res.status(500).json({msg: error.message});
+            next(error);
+        }
+    };
+
+    Update = async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const { first, last, age, info } = req.body;
+            const updateQry = `UPDATE players 
+            SET first = $1, last = $2, age = $3, info = $4 
+            WHERE id = $5 RETURNING *`;
+            const values = [first, last, age, info, id];
+            const updatePlayer = await pool.query(updateQry, values);
+            res.status(200).json(updatePlayer.rows[0]);
+        } catch (error) {
+            res.status(500).json({msg: error.message});
+            next(error);
+        }
+    };
+
+    Delete = async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const deleteQry = `
+                DELETE FROM players 
+                WHERE id = $1
+            `;
+            const values = [id];
+            const deletePlayer = await pool.query(deleteQry, values);
+            res.status(200).json(deletePlayer);
+        } catch (error) {
+            res.status(500).json({msg: error.message});
+            next(error);
+        }
+    };
 };
 
 export const PLAYER = new PlayerClass();
--- src/routes/PlayerRt.js
+++ src/routes/PlayerRt.js
@@ -4,5 +4,8 @@ import { PLAYER } from "../controllers/PlayerCon.js";
 export const PlayerRt = express.Router();
     PlayerRt.post("/", PLAYER.Create);
     PlayerRt.get("/", PLAYER.FetchAll);
+    PlayerRt.get("/:id", PLAYER.GetOne);
+    PlayerRt.put("/:id", PLAYER.Update);
+    PlayerRt.delete("/:id", PLAYER.Delete);
 
 
