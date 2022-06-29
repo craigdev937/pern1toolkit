7ff7c97f7debbd5c986b30c7d31b4428e57b52b9
Craig <indigomx9@gmail.com>
2022-06-28 20:51:43 -0400

    Initial setup of the front-end.

---
+++ client/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod voluptatibus, aliquid eaque commodi nisi dolor explicabo asperiores hic laudantium cupiditate reprehenderit itaque voluptatum odit? Excepturi ea enim minus voluptatem odio!</p>
+        </React.Fragment>
+    );
+};
+
+
